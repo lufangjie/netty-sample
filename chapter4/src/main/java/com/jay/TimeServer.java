c94ecf065f9553196e237e7245f4e93dd44ae053
Lu Fangjie <lufangjie44@hotmail.com>
2019-02-25 10:19:33 +0800

    use LineBasedFramsDecoder and StringDecoder to fix stick package with /n separator

---
+++ chapter4/src/main/java/com/jay/TimeServer.java
@@ -0,0 +1,67 @@
+package com.jay;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+/**
+ * @ClassName TimeServer
+ * @Description 使用Netty的半包解码器解决TCP粘包/拆包问题
+ * @Author lufangjie
+ * @Version 1.0
+ **/
+public class TimeServer {
+
+    public void bind(int port) throws Exception {
+        // 配置服务端的NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    // 设置创建的Channel为NioServerSocketChannel，对应nio库中的ServerSocketChannel类
+                    .channel(NioServerSocketChannel.class)
+                    // TCP参数
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    // 绑定I/O事件的处理类，
+                    .childHandler(new ChildChannelHandler());
+            // 绑定端口，同步等待成功
+            // ChannelFuture的功能类似Java中的java.util.concurrent.Future，主要用于异步操作的通知回调
+            ChannelFuture f = b.bind(port).sync();
+            // 进行阻塞，等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } finally {
+            // 释放资源
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    /**
+     * @ClassName ChildChannelHandler
+     * @Description 作用类似Reactor模式中的Handler类，主要用于处理网络I/O事件，例如记录日志，对消息进行解编码等
+     **/
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel ch) throws Exception {
+            // 解决TCP粘包/拆包问题
+            // 在TimeServerHandler之前新增两个解码器：LineBasedFrameDecoder和StringDecoder
+            ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            ch.pipeline().addLast(new StringDecoder());
+            ch.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        int port = 8080;
+        new TimeServer().bind(port);
+        System.out.println("等待端口关闭");
+    }
+}
---
+++ chapter4/src/main/java/com/jay/TimeServerHandler.java
@@ -0,0 +1,52 @@
+package com.jay;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+/**
+ * @ClassName TimeServerHandler
+ * @Description 模拟粘包/拆包问题
+ * @Author lufangjie
+ * @Version 1.0
+ **/
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    private int counter;
+
+//    @Override
+//    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+//        ByteBuf buf = (ByteBuf) msg;
+//        byte[] req = new byte[buf.readableBytes()];
+//        buf.readBytes(req);
+//        // 每读到一条消息后，就记一次树，然后发送应答消息给客户端。
+//        // 按照设计，服务端接收到的消息总数应该跟客户端发送的消息数相同，而且请求消息删除回车换行符后应该为“QUERY TIME ORDER”
+//        String body = new String(req, StandardCharsets.UTF_8).substring(0, req.length - System.getProperty("line.separator").length());
+//        System.out.println("The time server receive order : " + body + "; the counter is : " + ++counter);
+//        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+//        currentTime = currentTime + System.getProperty("line.separator");
+//        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+//        ctx.writeAndFlush(resp);
+//    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("The time server receive order : " + body + "; the counter is : " + ++counter);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
